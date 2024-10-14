b8e7b7167beaa28beb830bdd0fe937e6b478dff2
Marini23 <marina.smile1m@gmail.com>
2024-10-14 23:40:03 +0200

    add_modal

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Modal from 'bootstrap/js/dist/modal';
+
+// const myModalEl = document.querySelector('#myModal');
+
+// // const modalElement = document.getElementById('#addModal');
+// // const modal = new Modal(modalElement);
+// const myModal = new Modal(myModalEl);
+
+// // // To show the modal
+// // modal.show();
+
+// // // To hide the modal
+// // modal.hide();
+
+// document.querySelector('.btn-close').addEventListener('click', () => {
+//   console.log('close');
+//   myModal.hide();
+// });
